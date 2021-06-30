0da65eb979e7132a629966463e1bd4c69f3f4afa
Natalia Maximo <t-nmaximo@microsoft.com>
2021-06-30 09:09:02 -0400

    Pushed generated docs

--- src/quilla/__main__.py
+++
@@ -1,5 +0,0 @@
-import quilla  # pragma: no cover
-
-
-if __name__ == "__main__":  # pragma: no cover
-    quilla.run()
--- src/quilla/common/__init__.py
+++
@@ -1,5 +0,0 @@
-'''
-All shared bits of code that are used throughout much of the UIValidation package
-that don't have any better place to be. This encompasses all shared utility code
-such as the utility classes, enums, and exceptions used by this application.
-'''
